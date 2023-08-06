2cf00b8a9c7cc6939380d2a32ec18b1f6769baa1
ihsajjad <ihsajjad1@gmail.com>
2023-08-07 03:08:16 +0600

    added some responsive design to the book now page

--- src/pages/bookNow/bookNow/BookNow.jsx
+++ src/pages/bookNow/bookNow/BookNow.jsx
@@ -12,9 +12,10 @@ const BookNow = () => {
   console.log(rooms);
 
   return (
-    <div className="p-5 min-h-screen pt-8 bg-slate-100">
-      <div className="flex flex-row gap-5 ">
-        <div className=" w-3/12">
+    <div className="p-5 min-h-screen md:pt-8 bg-slate-100">
+      <div className="flex md:flex-row flex-col gap-5 ">
+        {/* Sidebar section */}
+        <div className="md:w-3/12">
           <div className="flex flex-col space-y-3 w-full border-2 border-[var(--main-color)] p-5 rounded bg-[var(--main-color)] sticky top-5">
             <h2 className="text-3xl font-bold text-center mb-3">Search</h2>
             <input
@@ -44,37 +45,36 @@ const BookNow = () => {
             <input type="submit" name="" id="" value="Search" className="" />
           </div>
         </div>
-        <div className="w-9/12 border-2 border-[var(--main-color)] rounded">
-          <h3 className="text-3xl text-center font-bold bg-[var(--main-color)] text-white py-2">
+        {/* Card container section */}
+        <div className="md:w-9/12 border-2 border-[var(--main-color)] rounded">
+          <h3 className="md:text-3xl text-2xl text-center font-bold bg-[var(--main-color)] text-white py-2">
             Available Rooms
           </h3>
-          <div className="p-3 bg-white">
-            <div className="grid grid-cols-3 gap-5">
-              {rooms.map((room) => (
-                <div key={room.id} className="custom-card">
-                  <figure>
-                    <img
-                      src={room?.imgs[0]}
-                      alt=""
-                      className="rounded-t h-48 w-full"
-                    />
-                  </figure>
-                  <div className="p-2 mt-auto">
-                    <h3 className="text-xl">{room.name}</h3>
-                    <h4 className="font-semibold">Amenities</h4>
-                    <ul>
-                      {room?.amenities.map((amenitie, i) => (
-                        <li key={i}>{amenitie}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="flex items-center justify-between border-t py-2 mt-auto">
-                    <p className="text-3xl">${room.price}</p>
-                    <button className="custom-btn-outline">Details</button>
-                  </div>
+          <div className="p-3 bg-white grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+            {rooms.map((room) => (
+              <div key={room.id} className="custom-card">
+                <figure>
+                  <img
+                    src={room?.imgs[0]}
+                    alt=""
+                    className="rounded-t h-48 w-full"
+                  />
+                </figure>
+                <div className="p-2 mt-auto">
+                  <h3 className="text-xl">{room.name}</h3>
+                  <h4 className="font-semibold">Amenities</h4>
+                  <ul>
+                    {room?.amenities.map((amenitie, i) => (
+                      <li key={i}>{amenitie}</li>
+                    ))}
+                  </ul>
                 </div>
-              ))}
-            </div>
+                <div className="flex items-center justify-between border-t py-2 mt-auto">
+                  <p className="text-3xl">${room.price}</p>
+                  <button className="custom-btn-outline">Details</button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
